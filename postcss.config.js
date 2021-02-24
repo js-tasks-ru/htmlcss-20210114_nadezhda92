e83232d740ee2ae2a5bc5621339b949c3c8e115a
Nadezhda Babicheva <nadezhda.babicheva@egamings.com>
2021-02-24 20:01:01 +0300

    scss and some fixes

--- postcss.config.js
+++ postcss.config.js
@@ -2,14 +2,14 @@ module.exports = {
     syntax: 'postcss-scss',
     map: { inline: true },
     plugins: [
-        require('precss')({
-            lookup: { disable: true },
-            properties: {
-                // Чтобы работали CSS custom properties, а не заменялись на статическое значение.
-                disable: true,
-                preserve: true,
-            },
-        }),
+        // require('precss')({
+        //     lookup: { disable: true },
+        //     properties: {
+        //         // Чтобы работали CSS custom properties, а не заменялись на статическое значение.
+        //         disable: true,
+        //         preserve: true,
+        //     },
+        // }),
         require('postcss-import'),
         require('postcss-nested'),
         require('autoprefixer'),
